1b6ae316bdb060f2d1d2f2970497d9c8e308d322
toplayro <gittoromik.06@gmail.com>
2024-05-19 18:58:00 +0600

    s

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -6,7 +6,7 @@ function cors(req, res, next) {
 
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
 
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     
     next();
 }
